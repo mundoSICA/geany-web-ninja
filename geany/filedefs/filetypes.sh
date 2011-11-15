004dddf4c1e1d773b5014f0c5b9ae54c1d8ea976
Fitorec <chanerec@gmail.com>
2011-11-15 07:57:19 -0600

    defindiendo los bloques de texto para shell

--- geany/filedefs/filetypes.sh
+++ geany/filedefs/filetypes.sh
@@ -1,23 +1,23 @@
 # For complete documentation of this file, please see Geany's main documentation
 [styling]
 # foreground;background;bold;italic
-default=0xffffff;0x1E1E1E;false;false
+default=0xFFFFFF;0x1E1E1E;false;false
 commentline=0x747474;0x1E1E1E;false;false
 number=0x7ECDFF;0x1E1E1E;false;false
-string=0xA18651;0x1E1E1E;false;false
-character=0xA18651;0x1E1E1E;false;false
 word=0xFFCB4F;0x1E1E1E;true;false
+string=0xFF5F28;0x1E1E1E;false;false
+character=0xFF5F28;0x1E1E1E;false;false
 operator=0x98BAC5;0x1E1E1E;false;false
-identifier=0xffffff;0x1E1E1E;false;false
+identifier=0xFFFFFF;0x1E1E1E;false;false
 backticks=0x30FF00;0x504250;false;false
 param=0x009f00;0x1E1E1E;false;false
 scalar==0x7ECDFF;0x1E1E1E;false;false
-
-
-
+error=0xF42F02;0x000000;false;false
+here_delim=0xAAFF57;0x1B1B1B;false;false
+here_q=0xAAFF57;0x1E1E1E;false;false
 
 [keywords]
-primary=break case continue do done elif else esac eval exit export fi for function goto if in integer return set shift then until while
+primary=break case continue do done elif else esac eval exit export fi for function goto if in integer return set shift then until while echo grep sed cat 
 
 
 [settings]
