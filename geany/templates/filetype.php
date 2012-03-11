74aad4d5f4c474877dd5ab61c11593679ed63c4a
Fitorec <chanerec@gmail.com>
2012-03-10 20:18:14 -0600

    Agregando plantilla para el cakePHP,agregando las etiquetas de php, revisando error en el ciclo for en php de los snippets

--- geany/templates/filetype.php
+++ geany/templates/filetype.php
@@ -1,17 +1,16 @@
 <?php
-{fileheader}?>
+/**
+ * Description
+ *
+ * PHP versions 4 and 5
+ *
+ * @category  
+ * @package   
+ * @author    {developer} - <{mail}>
+ * @copyright 2012-{year} The Authors
+ * @license   MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * @version   CVS: $Id: {filename} 296938 2010-03-27 16:16:25Z dufuz $
+ * @link      http://www.opensource.org/
+ * @since     File available since Release 0.1
+ */
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-
-<head>
-	<title>{untitled}</title>
-	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
-	<meta name="generator" content="{geanyversion}" />
-</head>
-
-<body>
-	
-</body>
-</html>
